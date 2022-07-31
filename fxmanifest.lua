2565687d5e1cc92efd753897239385aa62bbd908
polisek <107623238+polisek@users.noreply.github.com>
2022-07-31 15:53:14 +0200

    Add files via upload

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'adamant'
+games {"rdr3"}
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+
+description 'Polísek - Discord: Polisek#1558 -  '
+
+client_script {
+	'client/client.lua',
+	'config.lua',
+}
+
+server_script {
+	'server/server.lua',
+}
+
+lua54 'yes'
